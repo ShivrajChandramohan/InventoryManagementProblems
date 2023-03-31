ca5104769d8eede7f929f3fa13f6aee5919d414b
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-04-01 00:14:57 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace InventoryManagement
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Inventory problems");
+
+
+        }
+    }
+
+
+
+}
