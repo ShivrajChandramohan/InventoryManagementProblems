4c3e57407ef52828d5d1fe018aa2b38f13899963
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-04-01 00:41:48 +0530

    Deails

---
+++ Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement1
+{
+    public class InventoryDetails
+    {
+        public List<TypesOfRice> typesOfRice;
+        public List<TypesOfWheet> typesOfwheet;
+
+        public List<TypesOfPulses> typesOfPulses;
+
+    }
+
+    public class TypesOfRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+
+    }
+
+    public class TypesOfWheet
+    {
+        public string name;
+        public int weight;
+        public int price;
+
+    }
+
+    public class TypesOfPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+
+    }
+}
+
