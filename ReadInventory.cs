c015156498d9240a4632166849cd1269545e64a3
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-04-01 00:43:19 +0530

    Read

---
+++ ReadInventory.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement1
+{
+    public class ReadInventory
+    {
+        public InventoryDetails Read(string path)
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(path))
+                {
+                    string json = file.ReadToEnd();      ///read file and store in json///
+
+                    return JsonConvert.DeserializeObject<InventoryDetails>(json); ////convert stream of bytes in object///
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
